9268965dd9acb1ffb730e3822cee62b7511474e7
Chico Freitas <prof.ofreitas@gmail.com>
2023-02-14 12:47:44 -0300

    chore(image and fonts): updating images and fonts

--- src/components/CopyrightSection/index.jsx
+++ src/components/CopyrightSection/index.jsx
@@ -1,12 +1,13 @@
+import logo from "../../assets/vector.png"
 const Index = () => {
     return (
-        <div className="bg-[#221f2c] py-20 px-32 text-center text-white border-t border-lime-300">
-            <div>
+        <div className="bg-[#221f2c] py-10 px-32 text-center text-white border-t border-lime-300">
+            <div className="font-[Montserrat] my-4">
                 <h3>CNPJ: 28.633.929/0001-77 CICERONE VOX</h3>
             </div>
 
-            <div>
-                2023 <img src="" alt="Logo livepaco" /> Todos os direitos reservados
+            <div className="justify-center">
+                <span>&#169;</span> 2023 <img src={logo} alt="Logo livepaco" className="inline mx-3"/> Todos os direitos reservados
             </div>
 
         </div>
--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -3,13 +3,13 @@ import camera from "../../assets/vector@2x.png"
 
 const Index = () => {
     return (
-        <div className="bg-gradient-to-t from-lime-600 to-lime-400 py-20 px-40 text-white grid grid-cols-2 gap-10">
+        <div className="bg-[url('assets/bg-banner@2x.png')] bg-left-bottom py-20 px-40 text-white grid grid-cols-2 gap-10">
 
-            <div className="px-20 text-left max-w-lg">
+            <div className="px-20 text-left max-w-xl">
 
-                <h1 className="text-6xl font-bold my-8 uppercase">Live Paco</h1>
+                <h1 className="font-[AxiForma] text-6xl font-bold my-8 uppercase">Live Paco</h1>
 
-                <h2 className="text-4xl my-8">A solução de reunião mais descomplicada</h2>
+                <h2 className="font-[Montserrat] text-4xl my-8">A solução de reunião mais descomplicada</h2>
 
                 <div className="text-lg text-white flex gap-5">
         
@@ -19,7 +19,7 @@ const Index = () => {
                         </button>
                     </div>
         
-                    <div>
+                    <div className="font-[Montserrat]">
                         Dê o play e conheça como funciona a Live Paco
                     </div>
         
